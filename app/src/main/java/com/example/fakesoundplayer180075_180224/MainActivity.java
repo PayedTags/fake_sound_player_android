18243dde0bc98dc80dfbe7c4d8b4bf2a1bf9fae5
Railan <railansouzaguimaraes@gmail.com>
2021-01-25 21:14:50 +0000

    Intent
    Criei um intent para testar como vai ficar na segunda pagina, falta só ler o intent no MainActivity2,

--- app/src/main/java/com/example/fakesoundplayer180075_180224/MainActivity.java
+++ app/src/main/java/com/example/fakesoundplayer180075_180224/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.fakesoundplayer180075_180224;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btn_Next = findViewById(R.id.btn_Next);
+
+        btn_Next.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, MainActivity2.class);
+                intent.putExtra("Imagem","Capa1.jpg");
+                intent.putExtra("Autor", "Billie Eilish");
+                intent.putExtra("Musica", "Bad Guy");
+
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/fakesoundplayer180075_180224/Musica.java
+++ app/src/main/java/com/example/fakesoundplayer180075_180224/Musica.java
@@ -6,8 +6,24 @@ import java.util.Date;
 
 public class Musica {
 
+    private String autor;
     private String titulo;
     private Date segundos;
     private Image capa;
+
+    public Musica(String autor, String titulo, Date segundos, Image capa)
+    {
+        this.autor = autor;
+        this.titulo = titulo;
+        this.segundos = segundos;
+        this.capa = capa;
+    }
+
+    public Musica(String autor, String titulo, Date segundos)
+    {
+        this.autor = autor;
+        this.titulo = titulo;
+        this.segundos = segundos;
+    }
 
 }
