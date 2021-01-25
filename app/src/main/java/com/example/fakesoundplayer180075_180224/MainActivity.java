9f993f10165314030ecb8d38f7e172d8a0a2f494
PayedTags <tiagoferreira.tf648@gmail.com>
2021-01-25 19:15:07 +0000

    update 2
    Tratar do arraylist e do arrayadapter para a listview

--- app/src/main/java/com/example/fakesoundplayer180075_180224/MainActivity.java
+++ app/src/main/java/com/example/fakesoundplayer180075_180224/MainActivity.java
@@ -1,14 +1,31 @@
 package com.example.fakesoundplayer180075_180224;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import java.util.Date;
 import android.os.Bundle;
+import android.text.format.DateUtils;
+import android.widget.ArrayAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    static final long ONE_MINUTE_IN_MILLIS=60000;//millisecs
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ArrayList<Musica> playlist = new ArrayList<Musica>();
+
+        Musica paranoid = new Musica("I prevail", "Paranoid", new Date(10 * ONE_MINUTE_IN_MILLIS));
+
+        playlist.add(paranoid);
+
+        ArrayAdapter<Musica> arrayAdapter = new ArrayAdapter<Musica>(this,)
+
+
     }
 }
---
+++ app/src/main/java/com/example/fakesoundplayer180075_180224/Playlist.java
@@ -0,0 +1,7 @@
+package com.example.fakesoundplayer180075_180224;
+
+public class Playlist
+{
+
+
+}
