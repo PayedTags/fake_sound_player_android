bfbea961f736b2ca17139de979890634be0c8ca0
PayedTags <tiagoferreira.tf648@gmail.com>
2021-01-28 00:51:31 +0000

    Intent acabado
    Agora quando se clica numa musica, aquilo ira abrir a outra atividade, e nesse intent vai levar informaçao sobre a musica a tocar e sobre a musica escolhida

--- app/src/main/java/com/example/fakesoundplayer180075_180224/MainActivity.java
+++ app/src/main/java/com/example/fakesoundplayer180075_180224/MainActivity.java
@@ -4,11 +4,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import java.util.Date;
 
 import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.media.Image;
 import android.os.Bundle;
 import android.text.format.DateUtils;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.BaseAdapter;
 import android.widget.ImageView;
@@ -22,6 +24,7 @@ public class MainActivity extends AppCompatActivity {
 
     ArrayList<Musica> playlist = new ArrayList<Musica>();
     int count = 0;
+    int currentTime = 0; // variavel para guardar o tempo em que estamos na musica
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,11 +32,12 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Musica paranoid = new Musica("I prevail", "Paranoid", 145, R.drawable._36020b88ae8a1c11a5ce051e8318026);
+        Musica AnotherLife = new Musica("Montionless in white", "Another life", 202, R.drawable._9ed23aae5d5cee0646876189ad7dc06);
 
-
-        while(count < 20)
+        while(count < 10)
         {
             playlist.add(paranoid);
+            playlist.add(AnotherLife);
             count++;
         }
 
@@ -44,6 +48,29 @@ public class MainActivity extends AppCompatActivity {
 
         listViewMusicas.setAdapter(adapter);
 
+
+        listViewMusicas.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
+                Intent intent = new Intent(getApplicationContext(), MainActivity2.class);
+                intent.putExtra("Imagem", playlist.get(position).getCapa());
+                intent.putExtra("Autor", playlist.get(position).getAutor());
+                intent.putExtra("Titulo", playlist.get(position).getTitulo());
+                intent.putExtra("Segundos", playlist.get(position).getSegundos());
+                intent.putExtra("currentTime", currentTime); // para mandar o tempo que estamos da musica para a outra atividade
+                startActivity(intent);
+            }
+        });
+
+
+       /* listViewMusicas.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(getApplicationContext(), MainActivity2.class);
+                i.putExtra("currentTime", currentTime); // para mandar o tempo que estamos da musica para a outra atividade
+                startActivity(i);
+            }
+        });*/
 
     }
 
--- app/src/main/java/com/example/fakesoundplayer180075_180224/MainActivity2.java
+++ app/src/main/java/com/example/fakesoundplayer180075_180224/MainActivity2.java
@@ -2,13 +2,39 @@ package com.example.fakesoundplayer180075_180224;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity2 extends AppCompatActivity {
+
+    int currentTime = 0; // variavel para guardar o tempo em que estamos na musica
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+        Intent intent = getIntent();
+
+        currentTime = intent.getIntExtra("currentTime", 0); // para ir buscar o tempo que estamos da musica quando voltarmos da outra atividade;
+        int imagem = intent.getIntExtra("Imagem", 0);
+        String autor = intent.getStringExtra("Autor");
+        String titulo = intent.getStringExtra("Titulo");
+        int segundos = intent.getIntExtra("Segundos", 0);
+
+
+        Button buttonPlayer = findViewById(R.id.btn_player);
+
+        buttonPlayer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(getApplicationContext(), MainActivity.class);
+                i.putExtra("currentTime", currentTime); // para mandar o tempo que estamos da musica para a outra atividade (o footer)
+                startActivity(i);
+            }
+        });
+
     }
 }
