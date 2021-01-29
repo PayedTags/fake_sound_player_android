7cff9e2a420dd9d4086e575707e9a3bee460a0f5
Railan <77082777+RailanC@users.noreply.github.com>
2021-01-29 00:28:25 +0000

    MediaPlayer
    Terminei o MediaPlayer

--- app/src/main/java/com/example/fakesoundplayer180075_180224/MainActivity2.java
+++ app/src/main/java/com/example/fakesoundplayer180075_180224/MainActivity2.java
@@ -12,6 +12,8 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import static com.example.fakesoundplayer180075_180224.R.string.Play;
+
 public class MainActivity2 extends AppCompatActivity {
     MediaPlayer player;
     int currentTime = 0; // variavel para guardar o tempo em que estamos na musica
@@ -24,53 +26,49 @@ public class MainActivity2 extends AppCompatActivity {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         Intent intent = getIntent();
 
-        final TextView txt_titulo = findViewById(R.id.txtview_Titulo);
-        final ImageView img  = findViewById(R.id.img_Capa);
-        final TextView txt_Autor = findViewById(R.id.txv_Autor);
+        TextView txt_titulo = findViewById(R.id.txv_Titulo);
+        ImageView img  = findViewById(R.id.img_Capa);
+        TextView txt_Autor = findViewById(R.id.txv_Autor);
 
         currentTime = intent.getIntExtra("currentTime", 0); // para ir buscar o tempo que estamos da musica quando voltarmos da outra atividade;
         int imagem = intent.getIntExtra("Imagem", 0);
-        String autor = intent.getStringExtra("Autor");
+        String  autor = intent.getStringExtra("Autor");
         String titulo = intent.getStringExtra("Titulo");
         int segundos = intent.getIntExtra("Segundos", 0);
+        img.setImageResource(imagem);
 
-        txt_titulo.setText(titulo);
         txt_Autor.setText(autor);
-        
-        if(titulo.equals("Mood"))
-        {
-            player = MediaPlayer.create(this, R.raw.mood);
-            img.setImageResource(R.drawable._mood);
-        }
-
-        if(titulo.equals("Paranoid")){
-            player = MediaPlayer.create(this, R.raw.paranoid);
-            img.setImageResource(R.drawable._9ed23aae5d5cee0646876189ad7dc06);
-        }
-        if(titulo.equals("Another life")){
-            player = MediaPlayer.create(this, R.raw.another_life);
-            img.setImageResource(R.drawable._36020b88ae8a1c11a5ce051e8318026);
-        }
-        if(titulo.equals("Bang!!")){
-            player = MediaPlayer.create(this, R.raw.bang);
-            img.setImageResource(R.drawable._bang);
-        }
-
-        player.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
-            @Override
-            public void onCompletion(MediaPlayer mediaPlayer) {
-                stopPlayer();
-            }
-        });
-        player.start();
+        txt_titulo.setText(titulo);
         Button buttonPlayer = findViewById(R.id.btn_player);
-
+        buttonPlayer.setText(Play);
         buttonPlayer.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent i = new Intent(getApplicationContext(), MainActivity.class);
-                i.putExtra("currentTime", currentTime); // para mandar o tempo que estamos da musica para a outra atividade (o footer)
-                startActivity(i);
+                if(titulo.equals("Mood"))
+                {
+                    player = MediaPlayer.create(MainActivity2.this, R.raw.mood);
+                }
+
+                if(titulo.equals("Paranoid")){
+                    player = MediaPlayer.create(MainActivity2.this, R.raw.paranoid);
+                }
+
+                if(titulo.equals("Another life")){
+                    player = MediaPlayer.create(MainActivity2.this, R.raw.another_life);
+                }
+
+                if(titulo.equals("Bang!!")){
+                    player = MediaPlayer.create(MainActivity2.this, R.raw.bang);
+                }
+
+                player.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+                    @Override
+                    public void onCompletion(MediaPlayer mediaPlayer) {
+                        stopPlayer();
+                    }
+                });
+                player.start();
+
             }
         });
 
@@ -81,12 +79,15 @@ public class MainActivity2 extends AppCompatActivity {
         {
             player.release();
             player = null;
-            Toast.makeText(this, "MediaPlayer released", Toast.LENGTH_SHORT).show();
         }
     }
 
     @Override
     protected void onStop() {
+        currentTime = player.getCurrentPosition();
+        Intent i = new Intent(getApplicationContext(), MainActivity.class);
+        i.putExtra("currentTime", currentTime); // para mandar o tempo que estamos da musica para a outra atividade (o footer)
+        startActivity(i);
         super.onStop();
         stopPlayer();
     }
